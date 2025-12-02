f3432ec24a2c62ec5d1db84f97a627a6909c3d9f
Gustavo Cordova <guscordovaa06@gmail.com>
2025-12-01 17:54:55 -0700

    Agrega clase envio

---
+++ src/Envio.cpp
@@ -0,0 +1,119 @@
+//
+// Created by gusco on 12/1/2025.
+//
+
+#include "Envio.h"
+#include <string>
+#include <vector>
+#include "Cliente.h"
+#include "Producto.h"
+#include "Repartidor.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+Envio::Envio() {
+    this -> idEnvio = " ";
+    this -> estado = " ";
+    this -> productos;
+    this -> cantidad;
+    this -> repartidor;
+    this -> cliente;
+}
+
+Envio::Envio(string cIdEnvio, string cEstado, vector<Producto> cProductos, vector<int> cCantidad,
+             Repartidor cRepartidor, Cliente cCliente) {
+    this -> idEnvio = cIdEnvio;
+    this -> estado = cEstado;
+    this -> productos  = cProductos;
+    this -> cantidad = cCantidad;
+    this -> repartidor = cRepartidor;
+    this -> cliente = cCliente;
+}
+
+string Envio::getIdEnvio() {
+    return this -> idEnvio;
+}
+
+string Envio::getEstado() {
+    return this -> estado;
+}
+
+vector<Producto> Envio::getProductos() {
+    return this -> productos;
+}
+
+vector<int> Envio::getCantidad() {
+    return this -> cantidad;
+}
+
+Repartidor Envio::getRepartidor() {
+    return this -> repartidor;
+}
+
+Cliente Envio::getCliente() {
+    return this -> cliente;
+}
+
+void Envio::setIdEnvio(string cIdEnvio) {
+    this -> idEnvio = cIdEnvio;
+}
+
+void Envio::setProductos(vector<Producto> cProducto) {
+    this -> productos = cProducto;
+}
+
+void Envio::setCantidad(vector<int> cCantidad) {
+    this -> cantidad = cCantidad;
+}
+
+void Envio::setRepartidor(Repartidor cRepartidor) {
+    this -> repartidor = cRepartidor;
+}
+
+void Envio::setCliente(Cliente cCliente) {
+    this -> cliente = cCliente;
+}
+
+void Envio::agregarProducto(string nombre, float precio, int cCantidad) {
+    productos.emplace_back(nombre, precio);
+    cantidad.push_back(cCantidad);
+}
+
+void Envio::eliminarProducto(string nombre) {
+    for (int i = 0; i < productos.size(); i++){
+        if (productos[i].getNombre() == nombre){
+            productos.erase(productos.begin() + i);
+            cantidad.erase(cantidad.begin() + i);
+        }
+    }
+}
+
+double Envio::calculartotal() {
+    double total = 0;
+    for (int i = 0; i < productos.size(); i++){
+        total += productos[i].getPrecio() * cantidad[i];
+    }
+    return total;
+}
+
+void Envio::cambiarEstado(string cEstado) {
+    this -> estado = cEstado;
+}
+
+int Envio::leerFichero(string fichero) {
+
+}
+
+void Envio::mostrarinfo() {
+    cout << "El ID del envio es : " << this -> idEnvio << endl;
+    cout << "El estado de este envio es: " << this -> estado << endl;
+
+    for (int i = 0; i < productos.size(); i++){
+        cout << productos[i].getNombre() << "," << productos[i].getPrecio() << endl;
+        cout << "Existen" << cantidad[i] << " unidades de este producto" << endl;
+    }
+    cout << "El repartidor encargado de este envio es: " << repartidor.getNombre() << endl;
+    cout << "El cliente asociado con este envio es: " << cliente.getNombre() << endl;
+}
